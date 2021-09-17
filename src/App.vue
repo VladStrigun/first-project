7813488a737b3b357eb99b76ae6b2aeff11978c9
Vlad <zepra1222@gmail.com>
2021-09-17 12:14:50 +0300

    Home-1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <br />
+    {{ message }}
+    <HelloWorld msg="Здесь могла быть ваша реклама" :isShow="show" />
+    <input
+      type="text"
+      placeholder="text"
+      @keydown="blurMethod"
+      @keyup="focusMethod"
+      @blur="blurMethod"
+      @focus="focusMethod"
+    />
+    <!-- @click or v-on:click -->
+    <button @click="c = a + b">Click</button>
+    <button @click="doThat('Hello', $event)">Click</button>
+    {{ c }}
+    <calc />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from "./components/Calc.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Calc,
+  },
+  data: () => ({
+    message: "Привет, Vue",
+    show: false,
+    c: 0,
+    a: 1,
+    b: 1,
+  }),
+  methods: {
+    doThat(str, event) {
+      console.log(str, event);
+      this.c = 0;
+    },
+    blurMethod() {
+      console.log("blure");
+    },
+    focusMethod() {
+      console.log("focus");
+    },
+  },
+};
 </script>
 
 <style lang="scss">
